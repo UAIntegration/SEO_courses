3ae3f8418fba8c42a38b94014282b516b7bff075
zda1990@mail.ru <Plf50918950q>
2015-05-13 15:35:31 +0300

    resize_img

--- app/src/main/java/com/example/mystart/MyFragment3.java
+++ app/src/main/java/com/example/mystart/MyFragment3.java
@@ -8,14 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.Context;
-import android.os.Build;
-import android.view.Gravity;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 
 public class MyFragment3 extends Fragment {
@@ -29,7 +21,7 @@ public class MyFragment3 extends Fragment {
         ImageView tabl_01 = (ImageView) view.findViewById(R.id.imageindeks);
         tabl_01.setImageDrawable(getResources().getDrawable(R.drawable.o_0b8ee2));
         ImageView tabl_02 = (ImageView) view.findViewById(R.id.imageindeks2);
-        tabl_02.setImageDrawable(getResources().getDrawable(R.drawable.o_0b8ff2));
+        tabl_02.setImageDrawable(getResources().getDrawable(R.drawable.k_0b8ff2));
         return view;
     }
 }
